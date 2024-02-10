cd42026c2bb694ab13e8099b013f95a7501d0df0
babytayo <121583836+babytayo@users.noreply.github.com>
2024-02-10 11:02:41 +0100

    Work done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import avatarSection from "./components/AvatarSection";
+import { AvatarSection } from "./components/AvatarSection";
 import { Main } from "./components/Main";
-
-import "./App.css";
 
 export const App = () => {
 	return (
 		<div className="bg-overallBgImg bg-cover h-fit md:h-screen font-Poppins text-sm text-gray-800">
 			<div className=" bg-overBgOpacity h-fit md:h-full">
 				<div className=" grid md:grid-cols-2 h-full">
-					<avatarSection />
+					<AvatarSection />
 					<Main
 						about="I'm Oguneko Omotayo John by Name. A certified Electrical technician,
           and a Front-end website developer. I'm a fresh graduate of Psychology from the Nigerian Army university Biu, borno state."
--- src/components/AvatarSection.js
+++ src/components/AvatarSection.js
@@ -1,7 +1,46 @@
 import omotayopic from "./images/omotayopic.jpg";
 import { Icons } from "./IconSection";
 
-export default function avatarSection() {
+export const AvatarSection = () => {
+	// Avatar Text and buttons section
+	const AvatarTextandBtnsection = (props) => {
+		console.log(props);
+		const emailClickHandler = () =>
+			(window.location.href = "mailto: ogunekoomotayo@gmail.com");
+
+		const linkedinClickHandler = () =>
+			(window.location.href =
+				"https://www.linkedin.com/in/oguneko-omotayo-122884260");
+
+		return (
+			<div className=" pt-20 pl-2">
+				<h1 className="text-xl text-orange-600 font-semibold">{props.name}</h1>
+				<p className=" text-gold font-semibold">{props.stack}</p>
+				<div className="flex py-5  ">
+					<button
+						onClick={emailClickHandler}
+						className="profile_btn bg-goldencream text-veryLightcream  mr-3"
+					>
+						<i
+							classsName="fas fa-envelope text-sm mr-1 text-orange-600"
+							aria-hidden="true"
+						></i>
+						Email
+					</button>
+					<button
+						onClick={linkedinClickHandler}
+						className="profile_btn border-2 border-goldencream text-goldencream mr-3"
+					>
+						<i
+							className="fab fa-linkedin text-sm mr-1 text-orange-600"
+							aria-hidden="true"
+						></i>
+						Linkedin
+					</button>
+				</div>
+			</div>
+		);
+	};
 	return (
 		<div className=" w-full md:w-2/3 grid grid-cols-1 i justify-center mx-auto md:h-full bg-veryLightcream">
 			<div>
@@ -12,7 +51,7 @@ export default function avatarSection() {
 						</div>
 					</div>
 				</div>
-				<Mainavatarsection
+				<AvatarTextandBtnsection
 					name="Oguneko Omotayo John"
 					stack="Front-End Developer"
 				/>
@@ -20,44 +59,4 @@ export default function avatarSection() {
 			<Icons />
 		</div>
 	);
-}
-
-function Mainavatarsection(props) {
-	console.log(props);
-
-	const emailClickHandler = () =>
-		(window.location.href = "mailto: ogunekoomotayo@gmail.com");
-
-	const linkedinClickHandler = () =>
-		(window.location.href =
-			"https://www.linkedin.com/in/oguneko-omotayo-122884260");
-
-	return (
-		<div className=" pt-20 pl-2">
-			<h1 className="text-xl text-orange-600 font-semibold">{props.name}</h1>
-			<p className=" text-gold font-semibold">{props.stack}</p>
-			<div className="flex py-5  ">
-				<button
-					onClick={emailClickHandler}
-					className="profile_btn bg-goldencream text-veryLightcream  mr-3"
-				>
-					<i
-						className="fas fa-envelope text-sm mr-1 text-orange-600"
-						aria-hidden="true"
-					></i>
-					Email
-				</button>
-				<button
-					onClick={linkedinClickHandler}
-					className="profile_btn border-2 border-goldencream text-goldencream mr-3"
-				>
-					<i
-						className="fab fa-linkedin text-sm mr-1 text-orange-600"
-						aria-hidden="true"
-					></i>
-					Linkedin
-				</button>
-			</div>
-		</div>
-	);
-}
+};
